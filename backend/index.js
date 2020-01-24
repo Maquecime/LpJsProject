0742363a9754e4532775e707224c462f473f20a2
Maquecime <mdatin.contact@gmail.com>
2020-01-24 16:54:28 +0100

    rest api completed

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
-const uuid = require('uuid/v4');
+const uuid = require('uuidv4');
+const bodyParser = require('body-parser')
 
 const { Pool } = require('pg')
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.get('/api/rockets', (req, res) => {
@@ -16,8 +18,7 @@ app.get('/api/rockets', (req, res) => {
         password: 'db_password'
       })
     pool.query('SELECT * FROM rockets', (err, result) => {
-        pool.end()
-        
+        pool.end()        
             res.json(result.rows)
         });
         
@@ -30,24 +31,30 @@ app.post('/api/rockets', (req, res) =>{
         database: 'db_db',
         password: 'db_password'
     })
-    pool.query('INSERT INTO rockets(id,name,country,takeOffThrust) VALUES(?,?,?,?);', 
-    [uuid(),req.body.name, req.body.country, req.body.takeOffThrust], 
+    pool.query('INSERT INTO rockets(id,name,country,takeOffThrust) VALUES($1,$2,$3,$4);', 
+    [uuid.uuid(),req.body.name, req.body.country, req.body.takeOffThrust], 
     (err, result) => {
+        if(err != undefined) {
+            res.status(400).send(err);
+        }
         pool.end()
             res.json(result.rows)
     });
 })
 
-    app.put('/api/rockets/:id', (req, res) =>{
+app.put('/api/rockets/:id', (req, res) =>{
         const pool = new Pool({
         user: 'db_user',
         host: 'database',
         database: 'db_db',
         password: 'db_password'
     })
-    pool.query('UPDATE rockets SET name=?, country=?, takeOffThrust=? WHERE id=?;', 
-    [req.body.name, req.body.country, req.body.takeOffThrust,req.query.params.id], 
+    pool.query('UPDATE rockets SET name=$1, country=$2, takeOffThrust=$3 WHERE id=$4;', 
+    [req.body.name, req.body.country, req.body.takeOffThrust,req.params.id], 
     (err, result) => {
+        if(err != undefined) {
+            res.status(400).send(err);
+        }
         pool.end()
             res.json(result.rows)
     });
@@ -61,9 +68,30 @@ app.get('/api/rockets/:id', (req, res) =>{
         password: 'db_password'
     })
     pool.query('SELECT * FROM rockets WHERE id=$1;', 
-    [req.query.id], 
+    [req.params.id], 
     (err, result) => {
-        console.log(err);
+        if(err != undefined) {
+            res.status(404).send(err);
+        }
+        console.log(result);
+        pool.end()
+            res.json(result.rows)
+    });
+})
+
+app.delete('/api/rockets/:id', (req,res) => {
+    const pool = new Pool({
+        user:'db_user',
+        host:'database',
+        database: 'db_db',
+        password: 'db_password'
+    })
+    pool.query('DELETE FROM rockets WHERE id=$1;',
+    [req.params.id],
+    (err,result) => {
+        if(err != undefined) {
+            res.status(404).send(err);
+        }
         console.log(result);
         pool.end()
             res.json(result.rows)
